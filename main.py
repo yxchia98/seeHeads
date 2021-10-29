67d354ea710e30b14a91ea8cddb002cac71cde42
Yi Xuan <yxchia98@gmail.com>
2021-10-29 13:49:36 +0800

    added forks

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import fiftyone as fo
-import fiftyone.zoo as foz
+import torch
 
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5l, yolov5x, custom
+
+# Images
+img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
+video = './videos/people_walking.mp4'
+
+# Inference
+results = model(img)
+
+# Results
+# results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+results.show()
